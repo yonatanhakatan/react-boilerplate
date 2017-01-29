22132cd3a8a65af2c4460a1860f28c03859e86c5
Jonny Levy <iamjonnylevy@gmail.com>
2017-01-29 17:16:46 +0000

    fix(test): remove unused property
    Signed-off-by: Jonny Levy <iamjonnylevy@gmail.com>

--- assets/react/components/test/stories/Test.js
+++ assets/react/components/test/stories/Test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import Test from '../Test';
 
 storiesOf('Test', module)
