8ef712b183b2e790cc683f71dddcff784f07e86e
Jonny Levy <iamjonnylevy@gmail.com>
2017-01-29 17:16:21 +0000

    chore(storybook): use main webpack config
    Signed-off-by: Jonny Levy <iamjonnylevy@gmail.com>

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,1 @@
-const path = require('path');
-
-module.exports = {
-  module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        include: path.join(__dirname, '../assets/react'),
-        loaders: [
-          'style',
-          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-          'postcss',
-          'sass',
-        ],
-      },
-    ],
-  },
-};
+module.exports = require('../webpack.config.js');
